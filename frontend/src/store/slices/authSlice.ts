14728d22d9cade75b839032146e7b65f91aa06fb
monkeyatcode <donwknowhy1234@gmail.com>
2025-07-17 21:42:05 -0300

    epe

--- frontend/src/store/slices/authSlice.ts
+++ frontend/src/store/slices/authSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:4000/api'; /* process.env.REACT_APP_API_URL */
+const API_BASE_URL = /* 'http://localhost:4000/api'; */  process.env.REACT_APP_API_URL
 
 //||  
 
